c8d633b42a363cf5a29db4a908c1ae976bfd2bcd
Pham Nhu Son <phamnhusondolla@gmail.com>
2024-12-06 10:11:35 +0700

    Configure connection string to sql server database and create Db context to execute query

---
+++ Data/DataDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Razor.Language.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace HeartCoreAPI.Data
+{
+    public class DataDBContext :DbContext
+    {
+        public DataDBContext(DbContextOptions<DataDBContext> options) : base(options) { }   
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using HeartCoreAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// declare connection string called from appsettings.json
+
+builder.Services.AddDbContext<DataDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionSQL")));
 
 var app = builder.Build();
 
